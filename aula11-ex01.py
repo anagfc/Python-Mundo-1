531fe6da9e0d268d131a9b0956f120f23b08839f
anagfc <116309863+anagfc@users.noreply.github.com>
2023-01-09 01:33:15 -0300

    atualização da aula 11

--- aula11-ex01.py
+++ aula11-ex01.py
@@ -16,6 +16,8 @@ print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m.'.format(a, b))
 nome = 'Guanabara'
 print('Olá! Muito prazer em te conhecer, {}{}{}!'.format('\033[4;34m', nome, '\033[m'))
 
+
+# Dicionário - Será estudado nos próximos mundos
 cores = {'limpa': '\033[m',
          'azul': '\033[34m',
          'amarelo': '\033[33m',
